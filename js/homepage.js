eedd2df00935c62b25ce9834fe9f1692df7d8e30
Kyaw Thet Aung <billionairekta@gmail.com>
2020-07-27 00:34:51 +0630

    pset8 commit

---
+++ js/homepage.js
@@ -0,0 +1,32 @@
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function warning() {
+  alert('Are you sure?')
+}
+$('.carousel').carousel({
+  interval: 2000
+})
